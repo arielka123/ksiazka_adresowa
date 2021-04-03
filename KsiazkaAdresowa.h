35868b4ae466f3f5098e8194f77fb9d4d46777d5
Arleta Madej <arleta.madej@gmail.com>
2021-04-03 21:54:46 +0200

    Add registrated user to vector

---
+++ KsiazkaAdresowa.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include "Uzytkownik.h"
+
+using namespace std;
+
+class KsiazkaAdresowa
+{
+    int idZalogowanegoUzytkownika;
+    int idOstatniegoAdresata;
+    int idUsunietegoAdresata;
+
+    vector <Uzytkownik> uzytkownicy;
+
+    Uzytkownik podajDaneNowegoUzytkownika();
+    int pobierzIdNowegoUzytkownika();
+    bool czyIstniejeLogin(string login);
+
+public:
+    void rejestracjaUzytkownika();
+    void wypiszWszystkichUzytkownikow();
+};
